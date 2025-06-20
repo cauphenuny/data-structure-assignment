16418f2e608380c4ee4fa104d684209ab1a6fa51
Qiuzitao <qiuzitao23@mails.ucas.ac.cn>
2025-06-20 22:46:56 +0800

    fix(knights): change output type of main

--- knights_tour/src/main.cpp
+++ knights_tour/src/main.cpp
@@ -7,15 +7,35 @@ int main() {
     Algorithm modes;
     std::cin >> start_p.x >> start_p.y;
     std::cin >> choose_modes;
-    modes = choose_modes == 0 ? Algorithm::BRUTE_FORCE : Algorithm::HEURISTIC;
+
+    switch (choose_modes)
+    {
+    case 0:
+        modes = Algorithm::BRUTE_FORCE;
+        break;
+    case 1:
+        modes = Algorithm::HEURISTIC;
+        break;
+    case 2:
+        modes = Algorithm::HEURISTIC_INHANCER;
+        break;
+    default:
+        throw "unvalid modes";
+        break;
+    }
     
     std::vector<Path> results;
     results = solve(modes, start_p);
     
     // out_board.print_board();
+    size_t num_paths = results.size();
     size_t num_arrows = results[0].size();
-    for(int i=1; i < num_arrows; i++)
-        std::cout << "(" << results[0][i].start.x << ","<< results[0][i].start.y << ")";
+    for(int i = 0; i < (int)num_paths; i++) {
+        for(int j = 1; j < (int)num_arrows; j++)
+            std::cout << "(" << results[0][j].start.x << ","<< results[0][j].start.y << ")";
+        std::cout << std::endl;
+    }
+
 
     return 0;
 }
